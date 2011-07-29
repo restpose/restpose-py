89ac2b29f305d078f97fc72d3fbbbb24e3f8e2e4
Richard Boulton <richard@tartarus.org>
2011-07-29 19:48:30 +0100

    Test resources

---
+++ restpose/unittests/resource_test.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -
+#
+# This file is part of the restpose python module, released under the MIT
+# license.  See the COPYING file for more information.
+
+from .helpers import RestPoseTestCase
+from ..resource import RestPoseResource
+from ..errors import RestPoseError
+from restkit import ResourceError
+import sys
+
+class ResourceTest(RestPoseTestCase):
+    maxDiff = 10000
+
+    def setUp(self):
+        self.r = RestPoseResource('http://127.0.0.1:7777/')
+
+    def test_resource_errors(self):
+        """Check error handling in the resource
+
+        """
+        resp = self.r.get('/')
+
+        # Content returned by / isn't JSON
+        self.assertRaises(RestPoseError, getattr, resp, 'json')
+
+        self.assertEqual(resp.status_int, 200)
+        self.assertRaises(RestPoseError, resp.expect_status, 400)
