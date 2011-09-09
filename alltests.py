08da651286cf7ac8459a059a01831f75c08f80a7
Richard Boulton <richard@tartarus.org>
2011-09-09 10:22:18 +0100

    Fix invocation of subprocess.call to pass arguments as a list - makes doctests run again.

--- alltests.py
+++ alltests.py
@@ -7,7 +7,7 @@ import sys
 
 topdir = os.path.dirname(os.path.realpath(os.path.abspath(__file__)))
 os.chdir(topdir)
-retcode = subprocess.call("nosetests")
+retcode = subprocess.call(["nosetests"])
 if not retcode:
     os.chdir(os.path.join(topdir, "docs"))
-    subprocess.call("make", "doctest")
+    subprocess.call(["make", "doctest"])
