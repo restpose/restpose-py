d35d071fccd7c0e3b8c3f5d1086ad752a9467b8d
Richard Boulton <richard@tartarus.org>
2011-11-10 21:54:09 +0000

    Bump version number

--- restpose/version.py
+++ restpose/version.py
@@ -7,12 +7,12 @@
 """
 import six
 #: The development release suffix.  Will be an empty string for releases.
-dev_release = ""
+dev_release = "dev"
 
 #: The version of this restpose client, as a tuple of numbers.
 #: This does not include information about whether this is a development
 #: release.
-version_info = (0, 7, 6)
+version_info = (0, 7, 7)
 
 #: The version of this restpose client, as a string.
 #: This will have a development release suffix for development releases.
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ except ImportError:
     from distutils.core import setup, find_packages
 
 setup(name="Restpose",
-      version='0.7.6',
+      version='0.7.7dev',
       packages=find_packages(),
       include_package_data=True,
       author='Richard Boulton',
