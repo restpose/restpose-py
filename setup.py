f09928fd0f17bcffe2e17218253eaae813425f33
Richard Boulton <richard@tartarus.org>
2012-05-09 18:43:45 +0100

    Bump version number for dev

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ except ImportError:
     from distutils.core import setup, find_packages
 
 setup(name="Restpose",
-      version='0.7.7',
+      version='0.7.8dev',
       packages=find_packages(),
       include_package_data=True,
       author='Richard Boulton',
